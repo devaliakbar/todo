c0bf45376846add362ace56aed025edb040abdec
devaliakbar <dev.aliakbar@gmail.com>
2023-02-01 01:48:40 +0100

    Reafactored

--- lib/features/task/presentation/widget/task_history/task_history.dart
+++ lib/features/task/presentation/widget/task_history/task_history.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:tapped/tapped.dart';
 import 'package:todo/core/presentation/widget/main_screen_app_bar.dart';
+import 'package:todo/features/task/presentation/screen/task_detail_screen.dart';
 
 class TaskHistory extends StatelessWidget {
   const TaskHistory({super.key});
@@ -26,34 +28,44 @@ class TaskHistory extends StatelessWidget {
             itemCount: 5,
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
             physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) => Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(5)),
-              child: Column(
-                children: [
-                  Row(
-                    children: const [
-                      Expanded(
-                          child: Text(
-                        "Task Name",
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                      SizedBox(width: 15),
-                      Text("32 HRS"),
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Tapped(
+                onTap: () => Navigator.push(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.rightToLeft,
+                        child: const TaskDetailScreen())),
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: const [
+                          Expanded(
+                              child: Text(
+                            "Task Name",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          )),
+                          SizedBox(width: 15),
+                          Text("32 HRS"),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Created on : 22 Jun"),
+                          Text("Completed on : 27 Jun"),
+                        ],
+                      )
                     ],
                   ),
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text("Created on : 22 Jun"),
-                      Text("Completed on : 27 Jun"),
-                    ],
-                  )
-                ],
+                ),
               ),
             ),
           ),
